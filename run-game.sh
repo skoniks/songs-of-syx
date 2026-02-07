4f24431c08e15ff13abc924cea0cff1b079cdf28
skoniks <skolya16@ya.ru>
2026-02-07 06:09:05 +0300

    Initial commit

---
+++ run-game.sh
@@ -0,0 +1,10 @@
+./jre/bin/java \
+  -Dfml.earlyprogresswindow=false \
+  -Dfile.encoding=UTF-8 \
+  -Xms512m -Xmx4096m \
+  -XstartOnFirstThread \
+  -XX:+UseCompressedOops \
+  -XX:+UseSerialGC \
+  -server \
+  -cp "base/script/001_Tutorial.jar:base/script/000_Tutorial.jar:SongsOfSyx.jar" \
+  init.MainProcess
